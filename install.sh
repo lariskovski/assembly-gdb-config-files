122aeb827e87e54bc2fa248a705817eb49b37d1c
Larissa <37162993+lariskovski@users.noreply.github.com>
2024-08-08 19:16:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt-get install tmux curl git zsh
+sudo apt-get install tmux curl git zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp ./gdbinit ~/.gdbinit
 cp ./tmux.conf ~/.tmux.conf
